76a6056e2ec3bc5d7d7ead6423d721b0e4bd7475
Benjamin Diaz <bdiaz@whitestack.com>
2018-10-18 17:31:05 -0300

    Modifies deb pkg building to include postinst script
    stage-build now uses make instead of tox, to allow adding
    postinst script.
    Postinst script adds installation of dependencies through pip.
    Adds missing MANIFEST and stdeb.cfg.
    
    Signed-off-by: Benjamin Diaz <bdiaz@whitestack.com>
    Change-Id: I42849e0a3457745ad00a169128554cf3574bcb23

--- devops-stages/stage-build.sh
+++ devops-stages/stage-build.sh
@@ -16,7 +16,4 @@
 # moved to a Makefile in order to add post install. Needed for "pip3 install aiokafka", 
 # that is not available with a package
 
-#make clean package
-
-rm -rf deb_dist
-tox -e build
+make clean package
